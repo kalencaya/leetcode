21ff593f5aa431a7216f522d42bdb91e025f3104
wangqi <1942460489@qq.com>
2020-08-17 17:02:17 +0800

    350. Intersection of Two Arrays II easy

--- src/main/scala/cn/sliew/leetcode/hashtable/IntersectionofTwoArraysII350ScalaExample.scala
+++ src/main/scala/cn/sliew/leetcode/hashtable/IntersectionofTwoArraysII350ScalaExample.scala
@@ -1,5 +1,7 @@
 package cn.sliew.leetcode.hashtable
 
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * 350. Intersection of Two Arrays II easy
   * tags: Hash Table, Two Pointers, Binary Search, Sort
@@ -7,11 +9,33 @@ package cn.sliew.leetcode.hashtable
 object IntersectionofTwoArraysII350ScalaExample {
 
     def intersect(nums1: Array[Int], nums2: Array[Int]): Array[Int] = {
-
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return Array()
+        }
+        val result = new ArrayBuffer[Int]()
+        val sorted1 = nums1.sorted
+        val sorted2 = nums2.sorted
+        var index1 = 0
+        var index2 = 0
+        while (index1 < sorted1.length && index2 < sorted2.length) {
+            if (sorted1(index1) == sorted2(index2)) {
+                result += sorted1(index1)
+                index1 += 1
+                index2 += 1
+            } else if (sorted1(index1) > sorted2(index2)) {
+                index2 += 1
+            } else if (sorted1(index1) < sorted2(index2)) {
+                index1 += 1
+            }
+        }
+        result.toArray
     }
 
     def main(args: Array[String]): Unit = {
-
+        val nums1 = Array(-2147483648,1,2,3)
+        val nums2 = Array(1,-2147483648,-2147483648)
+        val result = intersect(nums1, nums2)
+        println(result.mkString("[", ",", "]"))
     }
 
 }
