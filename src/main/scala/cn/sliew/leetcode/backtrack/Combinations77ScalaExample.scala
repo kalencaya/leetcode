37ef35f4f4e9e07e4a3ac977af335ef811204a20
wangqi <1942460489@qq.com>
2020-09-08 16:56:33 +0800

    77.Combinations medium

--- src/main/scala/cn/sliew/leetcode/backtrack/Combinations77ScalaExample.scala
+++ src/main/scala/cn/sliew/leetcode/backtrack/Combinations77ScalaExample.scala
@@ -5,37 +5,35 @@ import scala.collection.mutable.ListBuffer
 
 /**
   * 77.Combinations medium
+  * tags: Backtracking
   */
 object Combinations77ScalaExample {
 
-
     def combine(n: Int, k: Int): List[List[Int]] = {
-        val nums = (1 to n) toArray
-        val combs = new ListBuffer[List[Int]]()
-        val comb = new mutable.HashSet[Int]()
-        backtrack(combs, comb, nums, 0, nums.length - 1, k)
-        combs.toList
+        val nums = (1 to n).toArray
+        val result = new ListBuffer[List[Int]]()
+        backtrack(0, nums.length, nums, k, new ListBuffer[Int], result)
+        result.toList
     }
 
-    private def backtrack(combs: ListBuffer[List[Int]], comb: mutable.HashSet[Int], nums: Array[Int], start: Int, end: Int, left: Int): Unit = {
+    private def backtrack(start: Int, end: Int, nums: Array[Int], left: Int, temp: mutable.Buffer[Int], result: mutable.Buffer[List[Int]]): Unit = {
         if (left == 0) {
-            combs += comb.toList
+            result += temp.toList
             return
         }
-
-        for (i <- start to end) {
-            comb += nums(i)
-            backtrack(combs, comb, nums, i + 1, end, left - 1)
-            comb -= nums(i)
+        for (i <- start until end) {
+            temp += nums(i)
+            backtrack(i + 1, end, nums, left - 1, temp, result)
+            temp -= nums(i)
         }
-
     }
+
 
     def main(args: Array[String]): Unit = {
         val n = 4
         val k = 2
         val result = combine(n, k)
-        result.foreach(list => println(list.mkString(", ")))
+        result.foreach(list => println(list.mkString("[", ",", "]")))
     }
 
 }
---
+++ src/main/scala/cn/sliew/leetcode/backtrack/RegularExpressionMatching10ScalaExample.scala
@@ -0,0 +1,19 @@
+package cn.sliew.leetcode.backtrack
+
+/**
+  * 10. Regular Expression Matching medium
+  * tags: Array, Dynamic Programming, Backtracking
+  */
+object RegularExpressionMatching10ScalaExample {
+
+    def isMatch(s: String, p: String): Boolean = {
+        false
+    }
+
+    def main(args: Array[String]): Unit = {
+        val s = "aa"
+        val p = "a*"
+        val result = isMatch(s, p)
+        println(result)
+    }
+}
