0b5e52320c4b77f2c38d500a1dfa8d47684111a0
wangqi <1942460489@qq.com>
2021-06-28 08:43:26 +0800

    143. Reorder List medium

--- src/main/scala/cn/sliew/leetcode/recursion/ReorderList143ScalaExample.scala
+++ src/main/scala/cn/sliew/leetcode/recursion/ReorderList143ScalaExample.scala
@@ -8,7 +8,21 @@ import cn.sliew.leetcode.ListNode
 object ReorderList143ScalaExample {
 
     def reorderList(head: ListNode): Unit = {
+        if (head == null || head.next == null || head.next.next == null) {
+            return
+        }
 
+        var prev = head
+        var last = head.next
+        while (last.next != null) {
+            prev = last
+            last = last.next
+        }
+        val next = head.next
+        head.next = last
+        head.next.next = next
+        prev.next = null
+        reorderList(next)
     }
 
     def main(args: Array[String]): Unit = {
@@ -16,6 +30,7 @@ object ReorderList143ScalaExample {
         head.next = new ListNode(2)
         head.next.next = new ListNode(3)
         head.next.next.next = new ListNode(4)
+        head.next.next.next.next = new ListNode(5)
 
         reorderList(head)
 
