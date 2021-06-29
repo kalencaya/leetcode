cc76f7397c5dfc38df2726e253be8d07a442fd07
wangqi <1942460489@qq.com>
2021-06-29 08:52:36 +0800

    234. Palindrome Linked List easy

---
+++ src/main/scala/cn/sliew/leetcode/recursion/PalindromeLinkedList234ScalaExample.scala
@@ -0,0 +1,18 @@
+package cn.sliew.leetcode.recursion
+
+import cn.sliew.leetcode.ListNode
+
+/**
+  * 234. Palindrome Linked List easy
+  */
+object PalindromeLinkedList234ScalaExample {
+
+    def isPalindrome(head: ListNode): Boolean = {
+        false
+
+    }
+
+    def main(args: Array[String]): Unit = {
+
+    }
+}
