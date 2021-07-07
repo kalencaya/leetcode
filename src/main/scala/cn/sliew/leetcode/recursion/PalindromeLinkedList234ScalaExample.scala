788f1df1a765fd2f64e26ce0579585f0e10b3aed
wangqi <1942460489@qq.com>
2021-07-07 08:42:28 +0800

    234. Palindrome Linked List easy

--- src/main/scala/cn/sliew/leetcode/recursion/PalindromeLinkedList234ScalaExample.scala
+++ src/main/scala/cn/sliew/leetcode/recursion/PalindromeLinkedList234ScalaExample.scala
@@ -2,17 +2,38 @@ package cn.sliew.leetcode.recursion
 
 import cn.sliew.leetcode.ListNode
 
+import scala.collection.mutable
+
 /**
   * 234. Palindrome Linked List easy
   */
 object PalindromeLinkedList234ScalaExample {
 
     def isPalindrome(head: ListNode): Boolean = {
+        if (head == null || head.next == null) {
+            return true
+        }
+        if (head.next.next == null) {
+            return head.x == head.next.x
+        }
+
+        val stack = new mutable.Stack()
+
         false
 
     }
 
-    def main(args: Array[String]): Unit = {
+    private def removeLast(head: ListNode): ListNode = {
 
+    }
+
+    def main(args: Array[String]): Unit = {
+        val head = new ListNode(1)
+        head.next = new ListNode(2)
+        head.next.next = new ListNode(2)
+        head.next.next.next = new ListNode(1)
+
+        val result = isPalindrome(head)
+        println(result)
     }
 }
