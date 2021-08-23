c374b9e023ad3c367727608aca40a51d62ae744c
wangqi <1942460489@qq.com>
2021-08-23 08:23:39 +0800

    1143. Longest Common Subsequence mediym

--- src/main/scala/cn/sliew/leetcode/dp/LongestCommonSubsequence1143ScalaExample.scala
+++ src/main/scala/cn/sliew/leetcode/dp/LongestCommonSubsequence1143ScalaExample.scala
@@ -17,23 +17,30 @@ object LongestCommonSubsequence1143ScalaExample {
             return ""
         }
 
-        find(text1, text2, 0, 0)
+        val dp = Array.ofDim[String](text1.length, text2.length)
+
+        find(text1, text2, 0, 0, dp)
     }
 
-    private def find(text1: String, text2: String, start1: Int, start2: Int): String = {
+    private def find(text1: String, text2: String, start1: Int, start2: Int, dp : Array[Array[String]]): String = {
         if (start1 == text1.length || start2 == text2.length) {
             return ""
         }
+        if (dp(start1)(start2) != null) {
+            return dp(start1)(start2)
+        }
         if (text1.charAt(start1) == text2.charAt(start2)) {
-            return text1.charAt(start1) + find(text1, text2, start1 + 1, start2 + 1)
-        }
-        val sub1 = find(text1, text2, start1 + 1, start2)
-        val sub2 = find(text1, text2, start1, start2 + 1)
-        if (sub1.length > sub2.length) {
-            sub1
+            dp(start1)(start2) = text1.charAt(start1) + find(text1, text2, start1 + 1, start2 + 1, dp)
         } else {
-            sub2
+            val sub1 = find(text1, text2, start1 + 1, start2, dp)
+            val sub2 = find(text1, text2, start1, start2 + 1, dp)
+            if (sub1.length > sub2.length) {
+                dp(start1)(start2) = sub1
+            } else {
+                dp(start1)(start2) = sub2
+            }
         }
+        dp(start1)(start2)
     }
 
     def main(args: Array[String]): Unit = {
