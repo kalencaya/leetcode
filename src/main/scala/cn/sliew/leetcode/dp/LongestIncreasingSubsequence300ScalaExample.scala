c16b305a0a7c78913777cf93d96732f6b29b9558
wangqi <1942460489@qq.com>
2021-08-23 08:56:34 +0800

    300. Longest Increasing Subsequence mediym

---
+++ src/main/scala/cn/sliew/leetcode/dp/LongestIncreasingSubsequence300ScalaExample.scala
@@ -0,0 +1,17 @@
+package cn.sliew.leetcode.dp
+
+/**
+  * 300. Longest Increasing Subsequence mediym
+  */
+object LongestIncreasingSubsequence300ScalaExample {
+
+    def lengthOfLIS(nums: Array[Int]): Int = {
+        ???
+    }
+
+    def main(args: Array[String]): Unit = {
+        val nums = Array(10,9,2,5,3,7,101,18)
+        val result = lengthOfLIS(nums)
+        println(result)
+    }
+}
