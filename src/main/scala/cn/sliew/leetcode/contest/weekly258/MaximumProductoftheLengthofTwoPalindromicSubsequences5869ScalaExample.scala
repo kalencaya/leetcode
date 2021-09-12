b9da4c8612455f361c07091b77b9a4154e13502e
wangqi <1942460489@qq.com>
2021-09-12 11:18:53 +0800

    weekly 258

---
+++ src/main/scala/cn/sliew/leetcode/contest/weekly258/MaximumProductoftheLengthofTwoPalindromicSubsequences5869ScalaExample.scala
@@ -0,0 +1,17 @@
+package cn.sliew.leetcode.contest.weekly258
+
+/**
+  * 5869. Maximum Product of the Length of Two Palindromic Subsequences medium
+  */
+object MaximumProductoftheLengthofTwoPalindromicSubsequences5869ScalaExample {
+
+    def maxProduct(s: String): Int = {
+        ???
+    }
+
+    def main(args: Array[String]): Unit = {
+        val s = "leetcodecom"
+        val result = maxProduct(s)
+        println(result)
+    }
+}
