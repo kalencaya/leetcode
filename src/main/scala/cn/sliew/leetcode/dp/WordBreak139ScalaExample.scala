4cde86a5fdeccecd7aca7f24c88fe63449b2170c
wangqi <1942460489@qq.com>
2021-09-15 22:31:40 +0800

    139. Word Break medium

---
+++ src/main/scala/cn/sliew/leetcode/dp/WordBreak139ScalaExample.scala
@@ -0,0 +1,39 @@
+package cn.sliew.leetcode.dp
+
+import scala.collection.mutable
+
+/**
+  * 139. Word Break medium
+  */
+object WordBreak139ScalaExample {
+
+    def wordBreak(s: String, wordDict: List[String]): Boolean = {
+        recurse(new mutable.HashMap[String, Boolean](), s, wordDict)
+    }
+
+    private def recurse(dp: mutable.HashMap[String, Boolean], s: String, wordDict: List[String]): Boolean = {
+        if (s.isEmpty) {
+            return true
+        }
+        if (dp.contains(s)) {
+            return dp.get(s).get
+        }
+
+        var canBreak = false
+        for (i <- 1 to s.length) {
+            val word = s.substring(0, i)
+            if (wordDict.contains(word) && recurse(dp, s.substring(i), wordDict)) {
+                canBreak = true
+            }
+        }
+        dp.put(s, canBreak)
+        dp.get(s).get
+    }
+
+    def main(args: Array[String]): Unit = {
+        val s = "catsandog"
+        val wordDict = List("cats","dog","sand","and","cat")
+        val result = wordBreak(s, wordDict)
+        println(result)
+    }
+}
