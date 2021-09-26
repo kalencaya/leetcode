e848079a74b78a694862b03a275469b210d1b130
wangqi <1942460489@qq.com>
2021-09-26 08:10:00 +0800

    5. Longest Palindromic Substring medium

--- src/main/scala/cn/sliew/leetcode/dp/LongestPalindromicSubstring5ScalaExample.scala
+++ src/main/scala/cn/sliew/leetcode/dp/LongestPalindromicSubstring5ScalaExample.scala
@@ -6,19 +6,33 @@ package cn.sliew.leetcode.dp
 object LongestPalindromicSubstring5ScalaExample {
 
     def longestPalindrome(s: String): String = {
-        var result = ""
-        val dp = Array.ofDim[Boolean](s.length, s.length)
-        for (i <- s.length - 1 to 0 by -1; j <- i until s.length) {
-            dp(i)(j) = (j - i < 3 || dp(i + 1)(j - 1)) && s.charAt(i) == s.charAt(j)
-            if (dp(i)(j) && (result == "" || j - i + 1 > result.length())) {
-                result = s.substring(i, j + 1)
+        var start = 0
+        var end = 0
+        for (i <- 0 until s.length) {
+            val length1 = expendAroundCenter(s, i, i)
+            val length2 = expendAroundCenter(s, i, i + 1)
+            val newLength = math.max(length1, length2)
+            if (newLength > `end` - start) {
+                start = i - (newLength - 1) / 2
+                `end` = i + newLength / 2
             }
         }
-        result
+        s.substring(start, `end` + 1)
+    }
+
+    private def expendAroundCenter(s: String, start: Int, end: Int): Int = {
+        var left = start
+        var right = `end`
+
+        while (left >= 0 && right < s.length && s.charAt(left) == s.charAt(right)) {
+            left -= 1
+            right += 1
+        }
+        right - left - 1
     }
 
     def main(args: Array[String]): Unit = {
-        val s = "ccc"
+        val s = "dccced"
         val result = longestPalindrome(s)
         println(result)
     }
